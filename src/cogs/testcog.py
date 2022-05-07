42079c7da9b8977bc0ef56ed6dd8d43e95f05c84
Gaelan McMillan <gaelan.mcmillan@gmail.com>
2022-05-07 16:46:33 -0400

    setting up rudimentary bot functionality

---
+++ src/cogs/testcog.py
@@ -0,0 +1,5 @@
+import os
+from dotenv import load_dotenv
+from nextcord.ext import commands
+
+load_dotenv()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 import os
+
 from dotenv import load_dotenv
+
+import nextcord
+from nextcord.ext import commands
+
+import cogs.testcog
 
 load_dotenv()
 
@@ -8,5 +14,18 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 TS_GUILD_ID = os.getenv("TS_GUILD_ID")
 PUBLIC_KEY = os.getenv("PUBLIC_KEY")
 
+intents = nextcord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+
+@bot.command(name="ping")
+async def SendMessage(ctx):
+    await ctx.send("pong")
+
 if __name__ == "__main__":
-    print(TOKEN)
+    bot.run(TOKEN)

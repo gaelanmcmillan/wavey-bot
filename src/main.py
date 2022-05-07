51bd867810412f47a81eb7f34646d15fb5a09c7e
Gaelan McMillan <gaelan.mcmillan@gmail.com>
2022-05-07 16:03:52 -0400

    remove SECRET, use dotenv instead

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-
+TOKEN = os.getenv("TOKEN")
+CLIENT_ID = os.getenv("CLIENT_ID")
+TS_GUILD_ID = os.getenv("TS_GUILD_ID")
+PUBLIC_KEY = os.getenv("PUBLIC_KEY")
 
 if __name__ == "__main__":
-    pass
+    print(TOKEN)
